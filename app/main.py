a815fe0eb96b148702e8c050a3d14584bde3c232
Walter Herold Veedla <walterheroldv@gmail.com>
2023-10-17 19:27:21 +0200

    Use context-manager instead of explicit close

--- app/main.py
+++ app/main.py
@@ -5,7 +5,6 @@ def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
-
     HOST = "localhost"
     PORT = 4221
     SOCKET_BUFSIZE = 4096
@@ -14,13 +13,14 @@ def main():
     # socket.create_server wraps create() and bind() into a single call
     # and also sets the socket listening mode
     server_socket = socket.create_server((HOST, PORT), reuse_port=True)
-    conn, addr = server_socket.accept() # wait for client
 
-    data = conn.recv(SOCKET_BUFSIZE)
-    conn.send(b"HTTP/1.1 200 OK\r\n\r\n")
-    conn.close()
+    conn, addr = server_socket.accept()
+    with conn:
+        data = conn.recv(SOCKET_BUFSIZE)
+        conn.send(b"HTTP/1.1 200 OK\r\n\r\n")
 
 
 
 if __name__ == "__main__":
     main()
+
