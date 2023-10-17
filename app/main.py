0ac880bbf74c6a0de408f6cdb69c8b49dc6daacd
Walter Herold Veedla <walterheroldv@gmail.com>
2023-10-17 19:03:17 +0200

    Send HTTP 200 OK after every message

--- app/main.py
+++ app/main.py
@@ -21,8 +21,7 @@ def main():
         data = conn.recv(SOCKET_BUFSIZE)
         if data == b'':
             break
-
-    conn.send(b"HTTP/1.1 200 OK\r\n\r\n")
+        conn.send(b"HTTP/1.1 200 OK\r\n\r\n")
 
 
 
