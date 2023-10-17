7bc79e5581471e691722e4d535beb30f3e0e6fa1
Walter Herold Veedla <walterheroldv@gmail.com>
2023-10-17 19:06:45 +0200

    Remove while loop

--- app/main.py
+++ app/main.py
@@ -16,12 +16,8 @@ def main():
     server_socket = socket.create_server((HOST, PORT), reuse_port=True)
     conn, addr = server_socket.accept() # wait for client
 
-    data = None
-    while True:
-        data = conn.recv(SOCKET_BUFSIZE)
-        if data == b'':
-            break
-        conn.send(b"HTTP/1.1 200 OK\r\n\r\n")
+    data = conn.recv(SOCKET_BUFSIZE)
+    conn.send(b"HTTP/1.1 200 OK\r\n\r\n")
     conn.close()
 
 
