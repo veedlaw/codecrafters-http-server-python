ce0c8750a00143e2dbd37e150299b64db48c754b
Walter Herold Veedla <walterheroldv@gmail.com>
2023-10-18 20:13:09 +0200

    Stage 5 completed

--- app/main.py
+++ app/main.py
@@ -19,6 +19,12 @@ def build_http_response(status_code: int, text: str) -> str:
     return response
 
 
+
+def parse_user_agent(user_agent_line):
+    _request_header, user_agent = user_agent_line.split(" ")
+    return user_agent
+
+
 def main():
 
     HOST = "localhost"
@@ -33,20 +39,28 @@ def main():
 
     conn, _ = server_socket.accept()
 
-
     with conn:
         data = conn.recv(SOCKET_BUFSIZE)
 
         http_msg_contents = data.decode().split("\r\n")
         msg_start_line = http_msg_contents[0]
-        request_type, path, protocol = msg_start_line.split(" ")
-        if path == "/":
-            conn.send(b"HTTP/1.1 200 OK\r\n\r\n")
-        elif path.startswith("/echo/"):
-            response = build_http_response(200, path[len("/echo/"):])
-            conn.send(response.encode())
+
+        http_method, request_target, http_version = msg_start_line.split(" ")
+        http_response = None
+
+        if request_target == "/":
+            http_response = "HTTP/1.1 200 OK\r\n\r\n"
+        elif request_target.startswith("/echo/"):
+            http_response = build_http_response(200, request_target[len("/echo/"):])
+        elif request_target == "/user-agent":
+            for line in http_msg_contents:
+                if line.startswith("User-Agent"):
+                    user_agent = parse_user_agent(line)
+                    http_response = build_http_response(200, user_agent)
         else:
-            conn.send(b"HTTP/1.1 404 Not Found\r\n\r\n")
+            http_response = "HTTP/1.1 404 Not Found\r\n\r\n"
+
+        conn.send(http_response.encode())
 
 
 
