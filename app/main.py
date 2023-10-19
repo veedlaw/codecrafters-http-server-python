64df257a75dfa1db92f5569ea03705eec07035ff
Walter Herold Veedla <walterheroldv@gmail.com>
2023-10-19 10:16:55 +0200

    Add threading to handle multiple requests simultaneously

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import socket
+import threading
 
 
 status_msg = {
@@ -25,28 +26,16 @@ def parse_user_agent(user_agent_line):
     return user_agent
 
 
-def main():
+def handle_request(client_conn, address, SOCKET_BUFSIZE = 4096):
 
-    HOST = "localhost"
-    PORT = 4221
-    SOCKET_BUFSIZE = 4096
-
-    # Making a TCP server involves a four-step process of making calls to the
-    # create(), bind(), listen() and accept() functions.
-    # socket.create_server wraps create() and bind() into a single call
-    # and also sets the socket listening mode
-    server_socket = socket.create_server((HOST, PORT), reuse_port=True)
-
-    conn, _ = server_socket.accept()
-
-    with conn:
-        data = conn.recv(SOCKET_BUFSIZE)
+    with client_conn:
+        data = client_conn.recv(SOCKET_BUFSIZE)
 
         http_msg_contents = data.decode().split("\r\n")
         msg_start_line = http_msg_contents[0]
 
         http_method, request_target, http_version = msg_start_line.split(" ")
-        http_response = None
+        http_response = ""
 
         if request_target == "/":
             http_response = "HTTP/1.1 200 OK\r\n\r\n"
@@ -57,10 +46,31 @@ def main():
                 if line.startswith("User-Agent"):
                     user_agent = parse_user_agent(line)
                     http_response = build_http_response(200, user_agent)
+                    break
         else:
             http_response = "HTTP/1.1 404 Not Found\r\n\r\n"
 
-        conn.send(http_response.encode())
+        client_conn.send(http_response.encode())
+
+
+
+
+def main():
+
+    HOST = "localhost"
+    PORT = 4221
+
+    # Making a TCP server involves a four-step process of making calls to the
+    # create(), bind(), listen() and accept() functions.
+    # socket.create_server wraps create() and bind() into a single call
+    # and also sets the socket listening mode
+    server_socket = socket.create_server((HOST, PORT), reuse_port=True)
+
+    while True:
+        conn, address = server_socket.accept()
+        thread = threading.Thread(target=handle_request, args=(conn, address))
+        thread.start()
+
 
 
 
